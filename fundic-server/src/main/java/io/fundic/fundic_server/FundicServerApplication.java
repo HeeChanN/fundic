d3448a4b3500587709f2534cccc633a6413b651f
Hee Chan <ahc70032@gmail.com>
2025-11-04 17:17:35 +0900

    프로젝트 초기화

---
+++ fundic-server/src/main/java/io/fundic/fundic_server/FundicServerApplication.java
@@ -0,0 +1,13 @@
+package io.fundic.fundic_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundicServerApplication.class, args);
+	}
+
+}
---
+++ fundic-server/src/test/java/io/fundic/fundic_server/FundicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fundic.fundic_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
